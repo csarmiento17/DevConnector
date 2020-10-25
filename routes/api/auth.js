bdffe2e58ff08b482f72f3f2377b760deb62890f
Carlopher Sarmiento <csarmiento@litera.com>
2020-10-26 00:11:26 +0800

    Register User and Authenticate

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -8,7 +8,7 @@ const config = require('config');
 const User = require('../../models/User');
 
 // @route   GET api/auth
-// @desc    Test route
+// @desc    Get user route
 // @access  Public
 router.get('/', auth, async (req, res) => {
   try {
