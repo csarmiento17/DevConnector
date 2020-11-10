49287165793827c21696281bcb91f0f750f5e8e4
Carlopher Sarmiento <csarmiento@litera.com>
2020-11-10 09:25:46 -0500

    Code update

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const app = express();
 // Connect Database
 connectDB();
 
-// app.get('/', (req, res) => res.send('API Running'));
-
 // Init Middleware
 app.use(express.json({ extended: false }));
 
