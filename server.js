7b6ced978d64a369dc700ff47e325ebae1fd40ce
Carlopher Sarmiento <csarmiento@litera.com>
2020-11-10 08:23:13 -0500

    Fixed path in server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-cosnt path = require('path');
+const path = require('path');
 const app = express();
 
 // Connect Database
@@ -17,17 +17,15 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-
 //Server static assets in production
-if(process.env.NODE_ENV === 'production'){
-    //Set static folder
-    app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
 
-    app.get('*', (req,res) =>{
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
-
 
 const PORT = process.env.PORT || 5000;
 
