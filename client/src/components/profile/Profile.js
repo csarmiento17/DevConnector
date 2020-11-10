309962bff635771e437b69f285cb974a97afb8e6
Carlopher Sarmiento <csarmiento@litera.com>
2020-11-10 08:42:09 -0500

    Added match.params.id in Profiles.js

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -17,7 +17,7 @@ const Profile = ({
 }) => {
   useEffect(() => {
     getProfileById(match.params.id);
-  }, [getProfileById]);
+  }, [getProfileById, match.params.id]);
   return (
     <Fragment>
       {profile === null || loading ? (
